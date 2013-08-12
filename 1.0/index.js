cc9c7b94d64437668bd192816d8fdaf12b240048
舒克 <shuke.cl@taobao.com>
2013-08-12 17:43:19 +0800

    modify _href

--- 1.0/index.js
+++ 1.0/index.js
@@ -238,7 +238,7 @@ KISSY.add(function (S , Node) {
                 var customCfg =_item.attr('data-navigator') ;
                 var _navigator = {};
                 var _href = /#[\d\D]*/gi.exec(_item.attr('href'));
-                var _toNode = S.one(_item.attr('href'));
+                var _toNode = S.one(_href);
                 if (_item.hasAttr('data-navigator')) {
                     customCfg =_item.attr('data-navigator');
                 }else{
