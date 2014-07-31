14c047bc8eec96640b7d878862cc5028449ddd35
弘树 <tiehang.lth@alibaba-inc.com>
2014-07-31 21:20:31 +0800

    sync daily version 2.0.0

---
+++ 2.0.0/README.md
@@ -0,0 +1,13 @@
+## pagesidebar
+
+* 版本：2.0.0
+* 教程：[http://gallery.kissyui.com/pagesidebar/2.0.0/guide/index.html](http://gallery.kissyui.com/pagesidebar/2.0.0/guide/index.html)
+* demo：[http://gallery.kissyui.com/pagesidebar/2.0.0/demo/index.html](http://gallery.kissyui.com/pagesidebar/2.0.0/demo/index.html)
+
+## changelog
+
+### V2.0.0
+
+    [!]
+
+
---
+++ 2.0.0/guide/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "PageNavigator",
+        "PageSidebar"
+    ],
+    "modules": [
+        "PageNavigator",
+        "PageSidebar"
+    ],
+    "allModules": [
+        {
+            "displayName": "PageNavigator",
+            "name": "PageNavigator"
+        },
+        {
+            "displayName": "PageSidebar",
+            "name": "PageSidebar"
+        }
+    ]
+} };
+});
